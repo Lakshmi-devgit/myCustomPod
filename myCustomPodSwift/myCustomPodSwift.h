81d903eca5e09353cdef98791bdbc2d49cd6f936
Lakshmi <lakshmi@Lakshmis-MacBook-Pro.local>
2019-09-23 20:11:55 +0530

    Initial pod setup

---
+++ myCustomPodSwift/myCustomPodSwift.h
@@ -0,0 +1,19 @@
+//
+//  myCustomPodSwift.h
+//  myCustomPodSwift
+//
+//  Created by Lakshmi on 23/09/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myCustomPodSwift.
+FOUNDATION_EXPORT double myCustomPodSwiftVersionNumber;
+
+//! Project version string for myCustomPodSwift.
+FOUNDATION_EXPORT const unsigned char myCustomPodSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myCustomPodSwift/PublicHeader.h>
+
+
