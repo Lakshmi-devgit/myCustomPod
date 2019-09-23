577d65dc3dee9503b893b661bec0ca854bcae51f
Lakshmi <lakshmi@Lakshmis-MacBook-Pro.local>
2019-09-23 20:26:10 +0530

    added test cases

---
+++ myCustomPodSwift/myCustomPod.swift
@@ -0,0 +1,25 @@
+//
+//  myCustomPod.swift
+//  myCustomPodSwift
+//
+//  Created by Lakshmi on 23/09/19.
+//  Copyright © 2019 Personal. All rights reserved.
+//
+
+public final class myCustomPod {
+    
+    let name = "myCustomPod"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+    public func mult(a: Int , b: Int) -> Int {
+        return a * b
+    }
+    
+}
--- myCustomPodSwiftTests/myCustomPodSwiftTests.swift
+++ myCustomPodSwiftTests/myCustomPodSwiftTests.swift
@@ -10,19 +10,30 @@ import XCTest
 @testable import myCustomPodSwift
 
 class myCustomPodSwiftTests: XCTestCase {
+    
+    var customPod : myCustomPod!
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        customPod = myCustomPod()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
+    func testAdd() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertEqual(customPod.add(a: 3, b: 4) , 7)
     }
+    func testSub() {
+        XCTAssertEqual(customPod.sub(a: 5, b: 2) , 3)
+    }
+    func testMult() {
+        XCTAssertEqual(customPod.mult(a: 2, b: 10) , 20)
+    }
+    
 
     func testPerformanceExample() {
         // This is an example of a performance test case.
